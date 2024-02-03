d433a9f843b7dc4785cef1a8806e4e3a27a2a027
cake <2429239979@qq.com>
2024-02-03 15:49:43 +0800

    修改：     grass.py 	修改：     requirements.txt

--- grass.py
+++ grass.py
@@ -1,24 +1,32 @@
 import requests
-from Crypto.Cipher import AES
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 from base64 import b64encode
 
 def password_encode(plain_text):
-    def zero_padding(data, block_size):
-        pad_len = block_size - (len(data) % block_size)
-        padding = b'\x00' * pad_len
-        return data + padding
-
-    def aes_cbc_encrypt(plain_text, key, iv):
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        cipher_text = cipher.encrypt(zero_padding(plain_text, AES.block_size))
-        return b64encode(cipher_text).decode('utf-8')  # 将字节串转换为字符串
-
     key = b'pigxpigxpigxpigx'  # 密钥
     iv = b'pigxpigxpigxpigx'  # 偏移量
     plain_text = plain_text.encode()  # 将字符串转换为字节串
 
-    encodedpassword = aes_cbc_encrypt(plain_text, key, iv)
-    return encodedpassword
+    # 创建一个 AES CBC cipher
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+
+    # 创建一个 encryptor
+    encryptor = cipher.encryptor()
+
+    # 计算 padding 长度
+    pad_len = 16 - (len(plain_text) % 16)
+
+    # 添加 padding
+    plain_text += b'\x00' * pad_len
+
+    # 加密
+    cipher_text = encryptor.update(plain_text) + encryptor.finalize()
+
+    return b64encode(cipher_text).decode('utf-8')  # 将字节串转换为字符串
+
 
 def get_access_token(username, password):
     password = password_encode(password)
@@ -39,3 +47,5 @@ def get_answer_paper(username, password, id, studentId, subjectId):
     print(response.json())
     json_response = response.json()
     return json_response['data']['answerSheetUrl']
+
+print(password_encode("123456"))
